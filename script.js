c65eefc9411b13224ddfbeade162e7d743a09ca1
Programer-akram <mah.akram202@gmail.com>
2022-09-25 20:06:44 +0600

    strting countdown bug fix

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    ++errorCount;
   }
 
   // check if given question text is equal to user typed text
@@ -67,7 +68,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken =parseInt(finishTime - startTime) / 1000;
 
   // show result modal
   resultModal.innerHTML = "";
@@ -107,13 +108,14 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = 'none';
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
